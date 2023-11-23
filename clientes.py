fc7fb82c8bb47bc1c222272526a958f6e96d1844
Gustavo Simes <a85879@ualg.pt>
2023-11-23 16:22:25 +0000

    apaguei os comentários

--- clientes.py
+++ clientes.py
@@ -2,14 +2,6 @@ from io_terminal import imprime_lista
 
 nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome clientes-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro clientes.py existente, deve apagar
-# todos os outros ficheiros clientes-*.py, e inclusive estes comentários
-
-# ...
-
 def cria_novo_cliente():
     """Pedir os dados de um novo cliente
 
