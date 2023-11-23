2e135247246336efba5ada58e8057f74715a5075
Simão Marcos <a85489@ualg.pt>
2023-11-23 16:18:19 +0000

    apaguei um comentário

--- io_ficheiros.py
+++ io_ficheiros.py
@@ -6,14 +6,6 @@ from faturas import nome_ficheiro_lista_de_faturas
 from veiculos import nome_ficheiro_lista_de_veiculos
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
-
-# ...
-
 def carrega_as_listas_dos_ficheiros():
     """TODO: documentação"""
 
