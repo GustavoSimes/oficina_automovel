32e5eecbaed0912925b341ffcd2ab6b495893159
Simão Marcos <a85489@ualg.pt>
2023-11-23 17:04:26 +0000

    adicionei uma função

--- io_terminal.py
+++ io_terminal.py
@@ -8,3 +8,33 @@ from tabulate import tabulate
 # todos os outros ficheiros io_terminal-*.py, e inclusive estes comentários
 
 # ...
+
+def imprime_lista(cabecalho, lista):
+    """Imprime a :attr:`lista` na forma de uma tabela com um cabeçalho
+
+    Recebe uma lista na forma [{"atrib1": valor1, "atrib2": valor2, ...},
+    {"atrib1": valor1, "atrib2": valor2, ...}, ...] e imprime no terminal uma tabela
+    na forma
+
+    ==  ======  ======
+    id  atrib1  atrib2
+    ==  ======  ======
+    0   valor1  valor2
+    1   ...      ...
+    ==  ======  ======
+
+    :param cabecalho: Cabecalho para a listagem
+    :param lista: Lista a ser impressa
+    """
+
+    print(cabecalho)
+
+    if (len(lista) == 0):
+        print("Lista vazia")
+    else:
+        # cabecalho da tabela
+        lista_a_imprimir = [["id"] + list(lista[0].keys())]
+        # dados
+        lista_a_imprimir.extend([[id] + list(d.values()) for id, d in enumerate(lista)])
+
+        print(tabulate(lista_a_imprimir, headers="firstrow", tablefmt='psql'))
