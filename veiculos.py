b22652d79cbc2aab7231ffb6415b2bc82d0bd41f
Gustavo Simes <a85879@ualg.pt>
2023-11-23 23:52:25 +0000

    aadicionei uma função ao código

--- veiculos.py
+++ veiculos.py
@@ -10,3 +10,20 @@ nome_ficheiro_lista_de_veiculos = "lista_de_veiculos.pk"
 # todos os outros ficheiros veiculos-*.py, e inclusive estes comentários
 
 # ...
+
+def cria_novo_veiculo():
+    """Pede ao utilizador para introduzir um novo veiculo
+
+    :return: dicionario com um veiculo na forma
+        {"marca": <<marca>>, "matricula": <<matricula>>, ...}
+    """
+
+    marca = input("marca? ")
+    matricula = input("matricula? ").upper()
+    # TODO: Pedir o resto dos dados do veiculo, e não esquecer de os guardar no dicionario
+    # ...
+
+    veiculo = {"marca": marca,
+               "matricula": matricula}
+
+    return veiculo
