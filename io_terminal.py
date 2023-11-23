805e1d97541361b91653d3e409b31be9d725c5f2
Simão Marcos <a85489@ualg.pt>
2023-11-23 17:07:39 +0000

    adicionei uma função

--- io_terminal.py
+++ io_terminal.py
@@ -43,3 +43,22 @@ def pause():
     """Faz uma pausa no programa e espera que o utilizador pressione ENTER"""
 
     input("Pressione ENTER para continuar...")
+
+    def pergunta_id(questao, lista, mostra_lista=False):
+    """TODO: documentação
+
+    :param questao:
+    :param lista:
+    :param mostra_lista:
+    :return:
+    """
+
+    if mostra_lista:
+        imprime_lista(cabecalho="", lista=lista)
+
+    while True:
+        id = int(input(questao))
+        if 0 <= id < len(lista):
+            return id
+        else:
+            print(f"id inexistente. Tente de novo. Valores admitidos {0} - {len(lista)}")
