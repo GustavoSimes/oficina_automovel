77c0cae17302d924794826c4e4cc2ec4c0e242d6
Gustavo Simes <a85879@ualg.pt>
2023-11-23 23:54:02 +0000

    adicionei outra função a este código

--- veiculos.py
+++ veiculos.py
@@ -27,3 +27,8 @@ def cria_novo_veiculo():
                "matricula": matricula}
 
     return veiculo
+
+def imprime_lista_de_veiculos(lista_de_veiculos):
+    """TODO: documentação"""
+
+    imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
