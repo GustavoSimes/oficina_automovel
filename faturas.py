ccf070f9b9fc83efa1b22f11bba17de7b17f4cdf
Abel Dias <a84809@ualg.pt>
2023-11-24 14:44:48 +0000

    adicionei "se deseja receber a fatura por email"

--- faturas.py
+++ faturas.py
@@ -21,13 +21,14 @@ def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
     id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
     id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
     descricao = pergunta_id(questao="Qual é a descrição?", lista=lista_de_veiculos, mostra_lista=True)
-
+    deseja_email = pergunta_id(questao="Deseja receber a fatura por email (S/N)?", lista=lista_de_veiculos, mostra_lista=True)
     # TODO: Pedir o resto dos dados da fatura, e não esquecer de os guardar no dicionario
     # ...
 
     fatura = {"cliente": id_cliente,
               "veiculo": id_veiculo,
               "data": date.today(),
-              "descrição": descricao}
+              "descrição": descricao,
+              "deseja_email": deseja_email}
 
     return fatura
