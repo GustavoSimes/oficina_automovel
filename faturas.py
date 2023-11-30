423666fd849c768425e27674ee8ab60780b23e09
Simão Marcos <a85489@ualg.pt>
2023-11-30 20:41:14 +0000

    apaguei um comentario

--- faturas.py
+++ faturas.py
@@ -5,8 +5,6 @@ from io_terminal import imprime_lista, pergunta_id
 nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 
 def imprime_lista_de_faturas(lista_de_faturas):
-    """TODO: documentação"""
-
     for fatura in lista_de_faturas:
         print(fatura)
     pass
