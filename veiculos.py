2577a7e29b4758c17accbcf749928bfe72a20b27
Simão Marcos <a85489@ualg.pt>
2023-12-01 15:04:28 +0000

    documentei uma função

--- veiculos.py
+++ veiculos.py
@@ -33,6 +33,31 @@ def cria_novo_veiculo():
     return veiculo
 
 def imprime_lista_de_veiculos(lista_de_veiculos):
+    """
+    Imprime uma lista de veículos formatada.
+
+    Esta função recebe uma lista de veículos e imprime suas informações de maneira formatada.
     
+    :param lista_de_veiculos: Uma lista de dicionários, onde cada dicionário representa um veículo.
+    :type lista_de_veiculos: list[dict]
+    
+    :return: None
+    :rtype: None
+
+    :Example:
+
+    >>> veiculos = [
+    ...     {"marca": "Toyota", "modelo": "Corolla", "matricula": "AB-123-CD", "ano": "2020", "classe": "3", "lugares": "5", "cor": "azul", "comprimento": "4.5", "cilindrada": "2000"},
+    ...     {"marca": "Honda", "modelo": "Civic", "matricula": "XY-789-ZW", "ano": "2019", "classe": "2", "lugares": "4", "cor": "vermelho", "comprimento": "4.2", "cilindrada": "1800"},
+    ...     # Adicione mais veículos conforme necessário
+    ... ]
+
+    >>> imprime_lista_de_veiculos(veiculos)
+    Lista de Veículos
+    -----------------
+    Marca: Toyota, Modelo: Corolla, Matrícula: AB-123-CD, Ano: 2020, Classe: 3, Lugares: 5, Cor: azul, Comprimento: 4.5, Cilindrada: 2000
+    Marca: Honda, Modelo: Civic, Matrícula: XY-789-ZW, Ano: 2019, Classe: 2, Lugares: 4, Cor: vermelho, Comprimento: 4.2, Cilindrada: 1800
+    ...
+    """
 
     imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
