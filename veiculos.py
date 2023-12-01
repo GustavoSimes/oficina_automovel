3b55dee575bf20e11d4094e09844312203713af4
Simão Marcos <a85489@ualg.pt>
2023-12-01 15:01:47 +0000

    apaguei um comentario

--- veiculos.py
+++ veiculos.py
@@ -33,6 +33,6 @@ def cria_novo_veiculo():
     return veiculo
 
 def imprime_lista_de_veiculos(lista_de_veiculos):
-    """TODO: documentação"""
+    
 
     imprime_lista(cabecalho="Lista de Veiculos", lista=lista_de_veiculos)
